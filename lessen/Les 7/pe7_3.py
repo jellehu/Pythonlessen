26169af0c89686c5c12195e277a069f8d1b8b850
AutoJelle <31843404+AutoJelle@users.noreply.github.com>
2018-10-01 00:11:20 +0200

    Les 7

--- lessen/Les 7/pe7_3.py
+++ lessen/Les 7/pe7_3.py
@@ -1,30 +1,55 @@
+# versie 2
 file = open("kaartnummers.txt")
 
-print("Deze file telt {} regels".format(len(file.readlines())))
-
-file.seek(0)
-
+fileRead = file.readlines()
+fileLength = len(fileRead)
 numberList = []
 
-for line in file.readlines():
-    kaartnummer, naam = line.split(',')
-    # print(kaartnummer)
-    numberList.append(kaartnummer)
-
+for line in fileRead:
+    value = line.split(',')
+    numberList.append(value[0])
 
 highestNumber = max(numberList)
+highestNumberIndex = numberList.index(highestNumber) + 1
 
-file.seek(0)
-
-tempLineNumber = 1
-lineNumber = 0
-
-for line in file.readlines():
-    if highestNumber in line:
-        lineNumber = tempLineNumber
-    else:
-        tempLineNumber += 1
-
-print("Het grootste kaartnummer is: {} en dat staat op regel {}".format(max(numberList), lineNumber))
+print("Deze file telt {} regels".format(fileLength))
+print("Het grootste kaartnummer is: {} en dat staat op regel {}".format(highestNumber, highestNumberIndex))
 
 file.close()
+
+
+
+
+
+
+# versie 1
+# file = open("kaartnummers.txt")
+#
+# print("Deze file telt {} regels".format(len(file.readlines())))
+#
+# file.seek(0)
+#
+# numberList = []
+#
+# for line in file.readlines():
+#     kaartnummer, naam = line.split(',')
+#     # print(kaartnummer)
+#     numberList.append(kaartnummer)
+#
+#
+# highestNumber = max(numberList)
+#
+# file.seek(0)
+#
+# tempLineNumber = 1
+# lineNumber = 0
+#
+# for line in file.readlines():
+#     if highestNumber in line:
+#         lineNumber = tempLineNumber
+#     else:
+#         tempLineNumber += 1
+#
+# print("Het grootste kaartnummer is: {} en dat staat op regel {}".format(max(numberList), lineNumber))
+#
+# file.close()
