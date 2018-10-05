af54a7b754b697a068b769808bab897495975a92
AutoJelle <31843404+AutoJelle@users.noreply.github.com>
2018-10-05 14:42:18 +0200

    Opdracht verbeterd

--- lessen/Jaar01/Periode01/Les08/pe8_2.py
+++ lessen/Jaar01/Periode01/Les08/pe8_2.py
@@ -3,11 +3,11 @@ list = eval(input("Geef een lijst met minimaal 10 strings: "))
 shortList = []
 
 for entry in list:
-    if len(entry) <= 4:
+    if len(entry) == 4:
         shortList.append(entry)
 
 
-print(shortList)
+print("De nieuw-gemaakte lijst met alle vier-letter strings is: {}".format(shortList))
 
 
 # ["boter", "kaas", "bier", "pizza", "thee", "drop", "koek", "cola", "boterham", "stamppot"]
